ebcbe7748ee7c0ef20a478ba1bef7691c5606e48
Step <stp.sip7@gmail.com>
2022-06-13 18:36:12 -0500

    proyecto inicial listo

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from "react-router-dom";
+
+import routes from "./index";
+
+import logo from "../logo.svg";
+
+export const Navigation = () => {
+  return (
+    <Suspense fallback={<span>Loading ...</span>}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink
+                    className={({ isActive }) => (isActive ? "nav-active" : "")}
+                    to={path}
+                    end
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <Routes>
+            {routes.map(({ path, component: Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+            {/* 👇 Esta es la diferencia */}
+            <Route path="/" element={<Navigate to={routes[0].path} />} />
+          </Routes>
+        </div>
+      </Router>
+    </Suspense>
+  );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+const routes = [
+  { name: "home", path: "/home", component: "home" },
+  { name: "users", path: "/users", component: "users" },
+  { name: "about", path: "/about", component: "about" },
+];
+
+export default routes;
